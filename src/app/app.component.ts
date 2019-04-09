f2a2e4c03a05ad41275662c4653183e54451fd13
webdatavisualdev <cli698638@gmail.com>
2019-04-09 19:06:11 +0300

    finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import {FullCalendarComponent} from '@fullcalendar/angular';
+import {BsModalRef, BsModalService} from 'ngx-bootstrap';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'TestFullCalendar';
+export class AppComponent implements OnInit {
+  calendarPlugins = [dayGridPlugin];
+  calendarEvents: any[];
+  @ViewChild('calendar') calendarComponent: FullCalendarComponent;
+
+  openedModal: BsModalRef;
+
+  form: FormGroup;
+
+  currentEvent: any;
+
+  constructor(private modalService: BsModalService) {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      date: new FormControl('', Validators.required)
+    });
+  }
+
+  ngOnInit(): void {
+    this.calendarEvents = [{title: 'test', date: '2019-04-10'}];
+  }
+
+  openModalCreate(modal) {
+    this.openedModal = this.modalService.show(modal);
+    /* const calendarApi = this.calendarComponent.getApi();
+     this.calendarEvents.push();
+     calendarApi.addEvent({title: 'test2', date: '2019-04-11'});
+     console.log(this.calendarEvents);*/
+  }
+
+  openModalEdit(event, modal) {
+    this.currentEvent = event.event;
+    this.form.get('title').setValue(event.event.title);
+    this.form.get('date').setValue(event.event.start);
+    this.openedModal = this.modalService.show(modal);
+  }
+
+  createEvent() {
+    console.log(this.form.value);
+    const calendarApi = this.calendarComponent.getApi();
+    const formValue = this.form.value;
+    calendarApi.addEvent({
+      title: formValue.title,
+      date: moment(formValue.date).format('YYYY-MM-DD')
+    });
+    this.form.reset();
+    this.openedModal.hide();
+  }
+
+  editEvent() {
+    this.currentEvent.setProp('title', this.form.get('title').value);
+    this.currentEvent.setStart(moment(this.form.get('date').value).format('YYYY-MM-DD'), {maintainDuration: true});
+    this.form.reset();
+    this.openedModal.hide();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {FullCalendarModule} from '@fullcalendar/angular';
+import {BsDatepickerModule, CollapseModule, ModalModule} from 'ngx-bootstrap';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FullCalendarModule,
+    BsDatepickerModule.forRoot(),
+    ModalModule.forRoot(),
+    CollapseModule,
+    ReactiveFormsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
