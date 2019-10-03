2bbb350a244a4b7f147894163511bcfbf53b5ff8
webdatavisualdev <cli698638@gmail.com>
2019-10-02 18:31:11 -0700

    Change UI

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,40 +29,43 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.calendarEvents = [{title: 'test', date: '2019-04-10'}];
+    this.calendarEvents = [{title: 'Birthday', date: moment(new Date()).format('YYYY-MM-DD')}];
   }
 
   openModalCreate(modal) {
     this.openedModal = this.modalService.show(modal);
-    /* const calendarApi = this.calendarComponent.getApi();
-     this.calendarEvents.push();
-     calendarApi.addEvent({title: 'test2', date: '2019-04-11'});
-     console.log(this.calendarEvents);*/
   }
 
   openModalEdit(event, modal) {
     this.currentEvent = event.event;
-    this.form.get('title').setValue(event.event.title);
-    this.form.get('date').setValue(event.event.start);
+    this.form.patchValue({
+      title: event.event.title,
+      date: event.event.start
+    });
     this.openedModal = this.modalService.show(modal);
   }
 
   createEvent() {
-    console.log(this.form.value);
     const calendarApi = this.calendarComponent.getApi();
     const formValue = this.form.value;
     calendarApi.addEvent({
       title: formValue.title,
       date: moment(formValue.date).format('YYYY-MM-DD')
     });
+    localStorage.setItem('events', JSON.stringify(this.calendarEvents));
     this.form.reset();
     this.openedModal.hide();
   }
 
   editEvent() {
-    this.currentEvent.setProp('title', this.form.get('title').value);
-    this.currentEvent.setStart(moment(this.form.get('date').value).format('YYYY-MM-DD'), {maintainDuration: true});
+    this.currentEvent.setProp('title', this.form.value.title);
+    this.currentEvent.setStart(moment(this.form.value.date).format('YYYY-MM-DD'), {maintainDuration: true});
     this.form.reset();
     this.openedModal.hide();
+  }
+
+  delete() {
+    this.openedModal.hide();
+    this.currentEvent.remove();
   }
 }
